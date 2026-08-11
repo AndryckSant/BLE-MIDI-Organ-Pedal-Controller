6b979ea3542a537059200089846a69afc6fb55bb
AndryckSant <andrycksilva29@outlook.com>
2026-08-11 18:13:40 -0300

    Feat: SSD1306 driver API implemented

--- Firmware/drivers/display_driver/ssd1306.h
+++
@@ -1,9 +0,0 @@
-#ifndef SSD1306_DRIVER_H
-#define SSD1306_DRIVER_H
-
-void ssd1306_init();
-void ssd1306_clear();
-void ssd1306_display_text(const char* text, int x, int y);
-void ssd1306_display_update();
-
-#endif // SSD1306_DRIVER_H
